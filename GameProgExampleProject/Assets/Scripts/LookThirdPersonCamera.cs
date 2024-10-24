3f37d50984aceb7ccb2ccaa886e22a7039ff8296
Johaxd0525 <johaxd0525@skola.goteborg.se>
2024-10-24 16:18:22 +0200

    School Lecture - Movement & Camera

---
+++ GameProgExampleProject/Assets/Scripts/LookThirdPersonCamera.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LookThirdPersonCamera : MonoBehaviour
+{
+
+    public Transform cameraFocus;
+    public Vector3 offset;
+   
+  
+    void Update()
+    {
+        transform.LookAt(cameraFocus);   
+        transform.position = Vector3.Lerp(transform.position, cameraFocus.position + offset, 0.8f * Time.deltaTime);
+    }
+}
---
+++ GameProgExampleProject/Assets/Scripts/MoveThirdPersonCamera.cs
@@ -0,0 +1,56 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public new Transform camera;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float rotaionSpeed = 600f;
+
+    [Header ("Ground Check")]
+    public float playerHeight = 2f;
+    public LayerMask whatisGround;
+
+    bool grounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+  
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 forwardVector = new Vector3(camera.forward.x, 0f, camera.forward.z).normalized;
+        Vector3 rightVector = new Vector3(camera.right.x, 0f, camera.right.z).normalized;
+
+        Vector3 moveVector = (forwardVector * vInput) + (rightVector * hInput);
+
+        if (moveVector.magnitude > 1)
+            moveVector = moveVector.normalized;
+
+        moveVector *= moveSpeed;
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight * 0.5f) + 0.2f, whatisGround);
+
+        float verticalSpeed = rb.linearVelocity.y;
+
+        if (grounded)
+            rb.linearVelocity = new Vector3(moveVector.x, verticalSpeed, moveVector.z);
+
+        if (moveVector.magnitude > 0.2f) 
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveVector);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotaionSpeed * Time.deltaTime);
+        }
+
+    }
+}
